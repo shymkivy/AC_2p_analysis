b12fd2fc9af7184b4154a5b15c7edf4155f8ee16
shymkivy <shymkivy@gmail.com>
2020-07-17 19:08:23 -0400

    hierarch clust

--- s3_mpl_functions/f_hierarch_clust.m
+++ s3_mpl_functions/f_hierarch_clust.m
@@ -5,24 +5,29 @@ if ~exist('dend_thresh', 'var')
 end
 method1 = 'ward';
 
+Z = linkage(data,method1);
+%Z = linkage(1-squareform(pdist(data,'cosine')),method1);
+
 f1 = figure;
-[~, ~, dend_order_si] = dendrogram(linkage(1-squareform(pdist(data,'cosine'))), 1000,'ColorThreshold',dend_thresh,'Orientation','left');
-[~, ~, dend_order] = dendrogram(linkage(data,'ward'), 1000,'ColorThreshold',dend_thresh,'Orientation','left');
+[~, ~, dend_order] = dendrogram(Z, 1000,'ColorThreshold',dend_thresh,'Orientation','left');
+close(f1);
 
 f2 = if_plot_trial_trial_image(1-squareform(pdist(data(dend_order,:), 'cosine')), 'cosine');
-if_plot_trial_trial_image(squareform(pdist(data(dend_order,:), 'euclidean')), 'euclidean');
+if_plot_trial_trial_image(1-squareform(pdist(data(dend_order,:), 'euclidean')), 'euclidean');
 %if_plot_trial_trial_image(squareform(pdist(data(dend_order,:), 'minkowski',1)), 'minkowski 1');
 %if_plot_trial_trial_image(squareform(pdist(data(dend_order,:), 'minkowski',2)), 'minkowski 2');
 
+
 num_clust = input('How many clusters?');
 
-if num_clust > 1
-    figure(f1);
-    [~, clust_ident, ~] = dendrogram(linkage(data,method1), num_clust,'ColorThreshold',1000,'Orientation','left');
-else
-    clust_ident = ones(size(data,1),1);
-end
-close(f1);
+clust_ident = cluster(Z, 'MaxClust', num_clust);
+
+% if num_clust > 1
+%     figure(f1);
+%     [~, clust_ident, ~] = dendrogram(linkage(data,method1), num_clust,'ColorThreshold',1000,'Orientation','left');
+% else
+%     clust_ident = ones(size(data,1),1);
+% end
 
 ord1 = clust_ident(dend_order);
 figure(f2); hold on;
